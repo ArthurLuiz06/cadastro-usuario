0669ed402e39729cdf9a2b1e7f152872074867bf
Arthur Luiz <luiz.arthur4545@gmail.com>
2025-10-14 21:00:28 -0300

    Update server.js

--- server.js
+++ server.js
@@ -3,20 +3,22 @@ const cors = require('cors');
 const app = express();
 const path = require('path');
 
+const usuarioRoutes = require('./src/routes/usuarios')
+
 //conectar ao banco
-const conexao = require('./db/conexao')
+const conexao = require('./src/db/conexao')
 
 // Middleware
 app.use(cors());
 app.use(express.json());
 
 // Servir os arquivos da pasta public
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname,'public')));
 
-// Rota de test
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'))
-});
+// Rota usuario
+app.use('/api', usuarioRoutes)
+
+
 
 // Iniciar o servidor
 app.listen(3000, () => console.log('Servidor rodando em http://localhost:3000'))
